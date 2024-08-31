98544718041e1983727cac0f27403bad66a1ca0d
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-08-31 11:56:41 +0530

    Create Model mapper based utility for entity dto mapping and vice versa

---
+++ src/main/java/org/example/notetaker/util/Mapping.java
@@ -0,0 +1,26 @@
+package org.example.notetaker.util;
+
+import org.example.notetaker.dto.NoteDTO;
+import org.example.notetaker.entity.NoteEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    //matters of NoteEntity and DTO
+    public NoteDTO convertToDTO(NoteEntity note) {
+        return modelMapper.map(note, NoteDTO.class);
+    }
+    public NoteEntity convertToEntity(NoteDTO dto) {
+        return modelMapper.map(dto, NoteEntity.class);
+    }
+    public List<NoteDTO> convertToDTO(List<NoteEntity> notes) {
+        return modelMapper.map(notes, List.class);
+    }
+}
