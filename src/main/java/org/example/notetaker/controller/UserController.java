2b6348e658a4ec46ee22cedf1a892f143f42e1f5
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-09-01 16:32:15 +0530

    Create util for Base64 handling

--- src/main/java/org/example/notetaker/controller/UserController.java
+++ src/main/java/org/example/notetaker/controller/UserController.java
@@ -3,7 +3,11 @@ package org.example.notetaker.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.notetaker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private final UserService userService;
+
+    //save
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> saveUser(
+            @RequestPart("firstName") String firstName,
+            @RequestPart ("lastName") String lastName,
+            @RequestPart ("email") String email,
+            @RequestPart ("password") String password,
+            @RequestPart("profilePic") String profilePic) {
+
+        // Handle profile pic
+
+
+
+    }
 }
--- src/main/java/org/example/notetaker/util/AppUtil.java
+++ src/main/java/org/example/notetaker/util/AppUtil.java
@@ -1,5 +1,6 @@
 package org.example.notetaker.util;
 
+import java.util.Base64;
 import java.util.UUID;
 
 public class AppUtil {
@@ -9,5 +10,9 @@ public class AppUtil {
 
     public static String createUserId(){
         return "USER-"+UUID.randomUUID();
+    }
+
+    public static String toBase64ProfilePic(String profilePic){
+        return Base64.getEncoder().encodeToString(profilePic.getBytes());
     }
 }
