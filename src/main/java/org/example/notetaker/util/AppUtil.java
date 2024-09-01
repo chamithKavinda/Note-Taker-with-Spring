8ce2cd978979ad3a998b51ac485c523cb1b45e38
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-09-01 15:30:23 +0530

    Util for generate the UserId is cofigued

--- src/main/java/org/example/notetaker/util/AppUtil.java
+++ src/main/java/org/example/notetaker/util/AppUtil.java
@@ -6,4 +6,8 @@ public class AppUtil {
     public static String createNoteId(){
         return "NOTE-"+UUID.randomUUID();
     }
+
+    public static String createUserId(){
+        return "USER-"+UUID.randomUUID();
+    }
 }
