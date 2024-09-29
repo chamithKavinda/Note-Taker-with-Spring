57e3c61943997c6bd29147ff7615391d65d85ea6
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-09-29 12:14:05 +0530

    Optimized the process of update a note with controller validation

--- src/main/java/org/example/notetaker/controller/NoteController.java
+++ src/main/java/org/example/notetaker/controller/NoteController.java
@@ -53,12 +53,13 @@ public class NoteController {
     @PatchMapping(value = "/{noteId}",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> updateNote(@PathVariable ("noteId") String noteId, @RequestBody NoteDTO note) {
         try {
+            if (note == null && (noteId == null || note.equals(""))){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
             noteService.updateNote(noteId, note);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }catch (NoteNotFound e){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }catch (Exception e){
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
